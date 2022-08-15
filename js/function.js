a3a5d0f9641a84ddce07e27f53887d989d97e5c7
Lucas Sessi <101476405+Lucas-Sessi@users.noreply.github.com>
2022-08-15 00:45:51 -0300

    4 projeto concluido

---
+++ js/function.js
@@ -0,0 +1,14 @@
+$(function(){
+
+    //menu mobile
+
+    menuMobile()
+    function menuMobile(){
+        $('.menu-mobile').click(function(){
+            $('#menu-Mobile').slideToggle(1000);
+        })
+    }
+
+
+
+})
